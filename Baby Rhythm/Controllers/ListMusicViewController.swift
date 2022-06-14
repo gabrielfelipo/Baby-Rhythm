6f5bab9bb8180893f88a3d3f44e19ccf98b47ae2
gabrielfelipo <gfrm@cin.ufpe.br>
2022-06-14 00:10:17 -0300

    Created a group for musics

--- Baby Rhythm/Controllers/ListMusicViewController.swift
+++ Baby Rhythm/Controllers/ListMusicViewController.swift
@@ -98,6 +98,8 @@ class ListMusicViewController: UIViewController, UICollectionViewDelegate, UICol
         }
         else {
             let urlString = Bundle.main.path(forResource: musicas[category ?? 0][sender.tag], ofType: "mp3")
+            print("AQUI CARAI")
+            print(Bundle.main)
             do {
                 try AVAudioSession.sharedInstance().setMode(.default)
                 try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
